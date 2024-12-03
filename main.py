07ac0b387c5cdaefa253719ea63c90fc8d30607d
Akash-Balaji003 <akashbalaji.tvr@gmail.com>
2024-12-04 01:12:29 +0530

    Reverting to working state

--- main.py
+++ main.py
@@ -3,37 +3,11 @@ import qrcode
 from io import BytesIO
 from fastapi import FastAPI, HTTPException, Query, Request
 import json
-import mysql.connector
-import aiomysql
 
 
-from DB_Interface import add_friend2, get_friends, get_friends2, get_profile_data, login_user, register_user, remove_friend
+from DB_Interface import add_friend2, get_friends, get_profile_data, login_user, register_user, remove_friend
 
 app = FastAPI()
-
-db_pool = None  # Declare a global variable for the connection pool
-
-
-@app.on_event("startup")
-async def startup_event():
-    global db_pool
-    db_pool = await aiomysql.create_pool(
-        host='swipe-mysql-server.mysql.database.azure.com',
-        port=3306,
-        user='Swipeadmin',
-        password='Swipeadmin123',
-        db='swipe_schema',
-        minsize=1,
-        maxsize=10
-    )
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    global db_pool
-    if db_pool:
-        db_pool.close()
-        await db_pool.wait_closed()
 
 @app.post("/register")
 async def register(request: Request):
@@ -80,24 +54,10 @@ async def removeFriend(data1: int = Query(...), data2: int = Query(...)):
     remove_friend_result = remove_friend(data1, data2)
     return remove_friend_result
 
-@app.get("/get-friend-v2")
-async def profile(data: int = Query(...)):
-    get_fnd_data = get_friends2(data)
-    return get_fnd_data
-
 @app.get("/get-friend")
 async def profile(data: int = Query(...)):
-    async with db_pool.acquire() as connection:
-        async with connection.cursor() as cursor:
-            query = """
-                SELECT users.username, users.common_name, user_profiles.profile_title 
-                FROM user_profiles
-                JOIN users ON user_profiles.user_id = users.id
-                WHERE user_profiles.profile_id IN (%s, %s, %s, %s);
-            """
-            await cursor.execute(query, (data, data, data, data))
-            result = await cursor.fetchall()
-    return {"friends": result}
+    get_fnd_data = get_friends(data)
+    return get_fnd_data
 
 @app.get("/test")
 async def test():
